4923e8b96381f1a35ca2c7371f7880ad56af1f5d
Jose Aguilar <jose.a.aguilar@accenture.com>
2022-11-04 13:20:39 -0600

    sonido dado agregado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,20 @@ export class AppComponent {
   textAltImg = '';
   dadoIzquierda: string = '../assets/img/dice1.png';
   dadoDerecha: string = '../assets/img/dice1.png';
-  dado1=1;
-  dado2=2;
+  dado1 = 1;
+  dado2 = 2;
+  audioDado = '../assets/audio/audio1.mp3';
+  button = document.querySelector('#btndado');
   tirarDados(): void {
-    this.dado1 = Math.round(Math.random() * 5) + 1;
-    this.dado2 = Math.round(Math.random() * 5) + 1;
-    this.dadoIzquierda = `../assets/img/dice${this.dado1}.png`;
-    this.dadoDerecha = `../assets/img/dice${this.dado2}.png`;
+    const audio = new Audio(this.audioDado);
+    audio.play();
+    console.log(this.button)
+    setTimeout(() => {
+      this.dado1 = Math.round(Math.random() * 5) + 1;
+      this.dado2 = Math.round(Math.random() * 5) + 1;
+      this.dadoIzquierda = `../assets/img/dice${this.dado1}.png`;
+      this.dadoDerecha = `../assets/img/dice${this.dado2}.png`;
+
+    }, 1500);
   }
 }
