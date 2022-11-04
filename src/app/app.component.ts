0944a5d04bff2be4fec77b24b8ff7d28b992098b
Jose Aguilar <jose.a.aguilar@accenture.com>
2022-11-04 13:03:34 -0600

    App dados finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'dados-app';
+  textAltImg = '';
+  dadoIzquierda: string = '../assets/img/dice1.png';
+  dadoDerecha: string = '../assets/img/dice1.png';
+  dado1=1;
+  dado2=2;
+  tirarDados(): void {
+    this.dado1 = Math.round(Math.random() * 5) + 1;
+    this.dado2 = Math.round(Math.random() * 5) + 1;
+    this.dadoIzquierda = `../assets/img/dice${this.dado1}.png`;
+    this.dadoDerecha = `../assets/img/dice${this.dado2}.png`;
+  }
 }
